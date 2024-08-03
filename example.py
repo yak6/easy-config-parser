cd17901ad2bb0646786ca4c106b0e73496154d09
yak <85031661+yak6@users.noreply.github.com>
2024-08-03 20:29:38 +0200

    Update example.py

--- example.py
+++ example.py
@@ -4,14 +4,14 @@ money = 300
 xp = 1000
 
 while True:
-    loader.replace("money", money, "data.as")
-    loader.replace("xp", xp, "data.as")
+    loader.replace("money", money, "data.anything")
+    loader.replace("xp", xp, "data.anything")
     choice = input("> ")
     if choice == "addmoney":
         money+=300
     elif choice == "addxp":
         xp+=300
     elif choice == "loadmoney":
-        print(loader.load("money", "data.as"))
+        print(loader.load("money", "data.anything"))
     elif choice == "loadxp":
-        print(loader.load("xp", "data.as"))
+        print(loader.load("xp", "data.anything"))
